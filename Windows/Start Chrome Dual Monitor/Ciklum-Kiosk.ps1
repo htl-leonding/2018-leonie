b1c3e6cc8f006250f60c0ec0e9eeb0e34d71ede6
Stefnotch <brandmairstefan@gmail.com>
2019-02-27 09:59:51 +0100

    Chrome 2 Monitors Starting

---
+++ Windows/Start Chrome Dual Monitor/Ciklum-Kiosk.ps1
@@ -0,0 +1,21 @@
+﻿######################################################
+##########            Kiosk Mode          ############
+######################################################
+#
+# Runs chrome and other apps in full-screen mode 
+# on predefined screens
+# ----------------------------------------------------
+
+$chromePath = 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'
+$chromeArguments = '--new-window --incognito'
+# if Window not moved (especially on machine start) - try increaing the delay. 
+$ChromeStartDelay = 3
+
+Set-Location $PSScriptRoot
+. .\HelperFunctions.ps1
+
+# Kill all running instances
+# &taskkill /im chrome* /F
+
+Chrome-Kiosk 'http://google.com' -MonitorNum 1 
+Chrome-Kiosk 'http://http://www.bbc.com/' -MonitorNum 2
---
+++ Windows/Start Chrome Dual Monitor/Readme.md
@@ -0,0 +1,23 @@
+# Chrome Starting Scripts
+
+## Scripts are from here
+
+[Stackoverflow Link](https://stackoverflow.com/questions/29294045/how-to-open-two-instances-of-chrome-kiosk-mode-in-different-displays-windows)
+
+## Running the scripts
+
+```powershell
+powershell -ExecutionPolicy ByPass -File .\Ciklum-Kiosk.ps1
+```
+
+[Source](https://stackoverflow.com/questions/4037939/powershell-says-execution-of-scripts-is-disabled-on-this-system)
+
+
+
+
+
+
+
+or double-click `Start - Kios - modify path`
+
+
