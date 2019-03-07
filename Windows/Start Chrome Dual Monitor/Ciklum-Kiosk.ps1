d4c5e6acdb8a80edc22132cbbe771e8870418c59
Stefnotch <brandmairstefan@gmail.com>
2019-03-07 10:36:17 +0100

    Remove "Restore Session" prompt
    https://stackoverflow.com/a/54833255/3492994

--- Windows/Start Chrome Dual Monitor/Ciklum-Kiosk.ps1
+++ Windows/Start Chrome Dual Monitor/Ciklum-Kiosk.ps1
@@ -6,6 +6,12 @@
 # on predefined screens
 # ----------------------------------------------------
 
+# Remove "Restore Session" prompt
+$c = Get-Content "$($env:localAppData)\Google\Chrome\User Data\Default\Preferences"
+if ($c.IndexOf('"exit_type":"Crashed"') -gt 0) {
+  $c.Replace('"exit_type":"Crashed"', '"exit_type":"Normal"').Replace('"exited_cleanly":false', '"exited_cleanly":true') | Set-Content "$($env:localAppData)\Google\Chrome\User Data\Default\Preferences" -NoNewLine
+}
+
 $chromePath = 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'
 $chromeArguments = '--new-window --incognito'
 # if Window not moved (especially on machine start) - try increaing the delay. 
